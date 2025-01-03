7107da56bec3dd8b2a49914404cf19531dac1932
Pranto Deb <rajprit0m@outlook.com>
2025-01-03 12:56:57 +0600

    [#]

--- Others/Algorithm.cpp
+++ Others/Algorithm.cpp
@@ -4,7 +4,14 @@ using namespace std;
 
 int main()
 {
-    //  
+    
+    int N;
+    cin >> N;
+    int factorial = 1;
+    for (int i = 1; i <= N; ++i) {
+        factorial = (factorial * i) % 10000;
+    }
+    cout << factorial << endl;
 
     return 0;
 }
