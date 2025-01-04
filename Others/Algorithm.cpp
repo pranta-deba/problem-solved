d67a0833cd740c2873ba6fc79288829779d6a473
Pranto Deb <rajprit0m@outlook.com>
2025-01-04 12:56:37 +0600

    [#]

--- Others/Algorithm.cpp
+++ Others/Algorithm.cpp
@@ -4,15 +4,7 @@ using namespace std;
 
 int main()
 {
-    //   1. linear search
-    //   2. binary search
-    //   3. bubble sort
-    //   4. selection sort
-    //   5. insertion sort
-    //   6. merge sort
-    //   7. quick sort
-    //   8. heap sort
-    
+    //  
 
     return 0;
 }
