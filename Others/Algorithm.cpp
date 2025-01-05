e6b1c539a60b9557535cf8e236c2ce33eaf15eb5
Pranto Deb <rajprit0m@outlook.com>
2025-01-05 12:55:41 +0600

    [#]

--- Others/Algorithm.cpp
+++ Others/Algorithm.cpp
@@ -4,11 +4,15 @@ using namespace std;
 
 int main()
 {
-    //   1. Selection Sort
-    //    2. Bubble Sort
-    //    3. Insertion Sort
-    //    4. Merge Sort
-    //    5. Quick Sort
+    //   1. linear search
+    //   2. binary search
+    //   3. bubble sort
+    //   4. selection sort
+    //   5. insertion sort
+    //   6. merge sort
+    //   7. quick sort
+    //   8. heap sort
+    
 
     return 0;
 }
