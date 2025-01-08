56746b7289672a727b5f41e9bedc2ca1e074acb4
Pranto Deb <rajprit0m@outlook.com>
2025-01-08 12:54:05 +0600

    [#]

--- Others/Algorithm.cpp
+++ Others/Algorithm.cpp
@@ -4,6 +4,19 @@ using namespace std;
 
 int main()
 {
-    cout<<"Hello World";
+    //  leap year
+    int year;
+    cout<<"Enter a year: ";
+    cin>>year;
+    if(year%4==0 && year%100!=0 || year%400==0)
+    {
+        cout<<year<<" is a leap year.";
+    }
+    else
+    {
+        cout<<year<<" is not a leap year.";
+    }
+    
+
     return 0;
 }
