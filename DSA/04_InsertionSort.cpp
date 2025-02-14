e5fd9c3602d67d2a20aca59cdc6ae64717343681
Pranto Deb <rajprit0m@outlook.com>
2025-02-14 22:38:57 +0600

    Insertion Sort

--- DSA/04_InsertionSort.cpp
+++ DSA/04_InsertionSort.cpp
@@ -2,19 +2,7 @@
 #include <vector>
 using namespace std;
 
-void insertionSort(vector<int> arr, int n)
-{
-    for (int i = 0; i < n; i++)
-    {
-        int current = i;
-        int prev = i + 1;
-        while (prev >= 0 && arr[prev] > current)
-        {
-            arr[prev]
-        }
-    }
-}
-void printArray(vector<int> arr, int n)
+void printArr(vector<int> arr, int n)
 {
     cout << "Sorted Array: ";
     for (int ele : arr)
@@ -22,6 +10,25 @@ void printArray(vector<int> arr, int n)
         cout << ele << " ";
     }
     cout << endl;
+}
+
+void insertionSort(vector<int> arr, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        int curr = arr[i];
+        int prev = i - 1;
+
+        while (prev >= 0 && arr[prev] > curr)
+        {
+            arr[prev + 1] = arr[prev];
+            prev--;
+        }
+
+        arr[prev + 1] = curr;
+    }
+
+    printArr(arr, n);
 }
 
 int main()
@@ -38,7 +45,6 @@ int main()
     }
 
     insertionSort(arr, n);
-    printArray(arr, n);
 
     return 0;
 }
