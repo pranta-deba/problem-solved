2cbcc6ceb004fc55ebfc784e2443ad55baae3a75
Pranto Deb <rajprit0m@outlook.com>
2025-02-15 20:49:31 +0600

    merge sort

--- DSA/05_MergeSort.cpp
+++ DSA/05_MergeSort.cpp
@@ -2,13 +2,23 @@
 #include <vector>
 using namespace std;
 
+void merge(vector<int> &arr, int st, int mid, int end)
+{
+    vector<int> temp;
+    int i = st, j = mid + 1;
+}
+
 void mergeSort(vector<int> &arr, int st, int end)
 {
 
     if (st < end)
     {
         int mid = st + (end - st) / 2;
-        mergeSort(arr, st, mid); // left half
+
+        mergeSort(arr, st, mid);      // left half
+        mergeSort(arr, mid + 1, end); // right half
+
+        merge(arr, st, mid, end);
     }
 }
 
