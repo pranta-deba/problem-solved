b99d12ac9eb2ca617fa1da86a8af5fffc27c676e
Pranto Deb <rajprit0m@outlook.com>
2025-02-18 23:07:23 +0600

    problem 11

--- Solved Problems/Problem_11.cpp
+++ Solved Problems/Problem_11.cpp
@@ -1,13 +1,24 @@
+// Greater of Two Numbers (Using Functions): Create a function that takes two numbers as input and returns the greater one.
 
-
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
-int main() {
+int greater(int num1, int num2)
+{
+    if (num1 > num2)
+    {
+        return num1;
+    }
+    else
+    {
+        return num2;
+    }
+}
 
-
+int main()
+{
 
     return 0;
 }
