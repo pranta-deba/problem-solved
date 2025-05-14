1c83915f9a1a46133bbf05f4d5976c6eb854497e
Pranto Deb <rajprit0m@outlook.com>
2025-05-14 23:19:38 +0600

    [DSA]:[Basic]:[Sum of Digits of a Number]

---
+++ DSA/Basic/09_Sum_Of_The_Digits_Of_A_Given_Number.cpp
@@ -0,0 +1,29 @@
+// Iterative C++ Code to find sum of digits
+
+#include <iostream>
+using namespace std;
+
+int sumOfDigits(int n)
+{
+    int sum = 0;
+    while (n != 0)
+    {
+
+        // Extract the last digit
+        int last = n % 10;
+
+        // Add last digit to sum
+        sum += last;
+
+        // Remove the last digit
+        n /= 10;
+    }
+    return sum;
+}
+
+int main()
+{
+    int n = 12345;
+    cout << sumOfDigits(n);
+    return 0;
+}
