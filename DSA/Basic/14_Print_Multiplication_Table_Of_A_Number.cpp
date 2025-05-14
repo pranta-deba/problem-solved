f8e11475c1735812fdedcaba26a4ab7f84d73791
Pranto Deb <rajprit0m@outlook.com>
2025-05-14 23:22:48 +0600

    [DSA]:[Basic]:[Program to print multiplication table of a number]

---
+++ DSA/Basic/14_Print_Multiplication_Table_Of_A_Number.cpp
@@ -0,0 +1,17 @@
+// CPP program to print table of a number
+#include <iostream>
+using namespace std;
+
+void printTable(int n)
+{
+    for (int i = 1; i <= 10; ++i)
+        cout << n << " * " << i << " = "
+             << n * i << endl;
+}
+
+int main()
+{
+    int n = 5;
+    printTable(n);
+    return 0;
+}
